b00f40c8e903877c59fe3bb9fa61e26054d4a4c2
Victor <vitocr_santos@hotmail.com>
2015-10-22 19:29:00 -0300

    Adicionando trabalho 2 de Computação Visual

--- Computação Visual/Victor/trabalho2/test.sh
+++ Computação Visual/Victor/trabalho2/test.sh
@@ -1,9 +1,11 @@
 # `make SRC=samples.cpp`
 
 echo "Testando..."
+# `make SRC=sample.cpp`
 
-for input in `ls ../../imgs/2015*r.jpg`
+for input in `ls ../../imgs/2015*.jpg`
 do
+	echo ${input}
 	$(./a.out ${input})
 done
 
